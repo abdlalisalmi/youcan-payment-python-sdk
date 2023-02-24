9bfb0378bf157d2b59827d0baadfe8e27560b2f3
abdlalisalmi <abdlalisalmi@gmail.com>
2023-02-24 17:10:57 +0100

    Updated -- Initialize the token object with Token class

--- youcanpay/client/__init__.py
+++ youcanpay/client/__init__.py
@@ -2,3 +2,4 @@
 
 from .api_service import APIService
 from .keys import Keys
+from .token import Token
--- youcanpay/youcanpay.py
+++ youcanpay/youcanpay.py
@@ -1,6 +1,6 @@
 import os
 from .client import APIService
-from .client import Keys
+from .client import Keys, Token
 
 
 class YouCanPay:
@@ -12,6 +12,9 @@ class YouCanPay:
     api_service = None
 
     def __init__(self, public_key: str, private_key: str, sandbox_mode=False) -> None:
+        # Initialize the api service
         self.api_service = APIService(public_key, private_key, sandbox_mode)
-
+        # Initialize the keys object
         self.keys = Keys(self.api_service)
+        # Initialize the token object
+        self.token = Token(self.api_service)
