bc50b5a8cdee409c775fbe6de212989cbd75bd27
abdlalisalmi <abdlalisalmi@gmail.com>
2023-02-24 17:08:09 +0100

    Added -- Add more exceptions [BadRequestError, UnauthorizedError, UnprocessableEntityError, NotFoundError, UnknownError]

--- youcanpay/exceptions.py
+++ youcanpay/exceptions.py
@@ -11,17 +11,61 @@ class BaseException(Exception):
         return self.message
 
 
+class BadRequestError(BaseException):
+
+    """Exception raised for bad requests."""
+
+    msg = 'Bad request. Please check the request and try again!'
+
+    def __init__(self, message=None):
+        super().__init__(message if message else self.msg)
+
+
+class UnauthorizedError(BaseException):
+
+    """Exception raised for unauthorized requests."""
+
+    msg = 'Unauthorized request. Please check the API keys and try again!'
+
+    def __init__(self, message=None):
+        super().__init__(message if message else self.msg)
+
+
+class UnprocessableEntityError(BaseException):
+
+    """Exception raised for unprocessable requests."""
+
+    msg = 'Unprocessable request. Please check the request and try again!'
+
+    def __init__(self, message=None):
+        super().__init__(message if message else self.msg)
+
+
+class NotFoundError(BaseException):
+
+    """Exception raised for not found requests."""
+
+    msg = 'Not found. Please check the request and try again!'
+
+    def __init__(self, message=None):
+        super().__init__(message if message else self.msg)
+
+
 class InternalServerError(BaseException):
 
     """Exception raised for internal server errors."""
 
     msg = 'Internal error from server. Support has been notified. Please try again!'
 
-    def __init__(self, message=msg):
-        super().__init__(message)
+    def __init__(self, message=None):
+        super().__init__(message if message else self.msg)
 
 
-class InvalidRequestError(BaseException):
+class UnknownError(BaseException):
 
-    """Exception raised for invalid requests."""
-    pass
+    """Exception raised for unknown errors."""
+
+    msg = 'Unknown error. Please try again!'
+
+    def __init__(self, message=None):
+        super().__init__(message if message else self.msg)
