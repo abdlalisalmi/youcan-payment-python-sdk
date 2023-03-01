328b419a7581e02b01aa0aa83608bc6fc3a78b23
abdlalisalmi <abdlalisalmi@gmail.com>
2023-03-01 23:33:23 +0100

    Added -- add get_payment_url() method that generate payment url

--- youcanpay/client/token.py
+++ youcanpay/client/token.py
@@ -5,7 +5,7 @@ class Token:
 
     """Token class to handle token related operations"""
 
-    BASE_ENDPOINT = 'tokenize'
+    BASE_ENDPOINT = "tokenize"
 
     def __init__(self, api_service) -> None:
         self.api_service = api_service
@@ -13,30 +13,35 @@ class Token:
 
         self.id = None
 
-    def create(self,
-               orderId: str,
-               amount: str,
-               currency: str,
-               customer_ip: str,
-               success_url: str = None,
-               error_url: str = None,
-               customer_info: list = [],
-               metadata: list = []
-               ):
+    def create(
+        self,
+        orderId: str,
+        amount: str,
+        currency: str,
+        customer_ip: str,
+        success_url: str = None,
+        error_url: str = None,
+        customer_info: list = [],
+        metadata: list = [],
+    ):
         data = {
-            'pri_key': self.api_service.get_private_key(),
-            'order_id': orderId,
-            'amount': amount,
-            'currency': currency,
-            'customer_ip': customer_ip,
-            'success_url': success_url,
-            'error_url': error_url,
-            'customer': customer_info,
-            'metadata': metadata,
+            "pri_key": self.api_service.get_private_key(),
+            "order_id": orderId,
+            "amount": amount,
+            "currency": currency,
+            "customer_ip": customer_ip,
+            "success_url": success_url,
+            "error_url": error_url,
+            "customer": customer_info,
+            "metadata": metadata,
         }
         res = self.api_service.post(self.BASE_ENDPOINT, data=data)
         res = self.api_service.assert_response(res)
-        self.id = res.get('token', {}).get('id', None)
+        self.id = res.get("token", {}).get("id", None)
         if not self.id:
             raise exceptions.UnknownError
         return self
+
+    def get_payment_url(self, lang="en"):
+        sandbox = "sandbox/" if self.api_service.is_sandbox_mode else ""
+        return f"{self.api_service.base_api_url}{sandbox}payment-form/{self.id}?lang={lang}"
